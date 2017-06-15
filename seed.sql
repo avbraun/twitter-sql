d4c6bb026704d76cd57e8d685a5da6c80839bb68
Connie Lee <connie.lee0317@gmail.com>
2017-06-15 11:04:20 -0500

    Shuffle order of tweet inserts
    So user ids do not match up perfectly with tweet ids - forces students to confront ambiguous id column on join between users and tweets

--- seed.sql
+++ seed.sql
@@ -10,14 +10,14 @@ INSERT INTO users (name, picture_url) VALUES ('Grace Hopper',        'http://i.i
 INSERT INTO users (name, picture_url) VALUES ('Kanye West',          'http://i.imgur.com/MItGWVS.jpg');
 INSERT INTO users (name, picture_url) VALUES ('Taylor Swift',        'http://i.imgur.com/JKInSVz.jpg');
 
-INSERT INTO tweets (user_id, content) VALUES ((SELECT id from users where name='Tom Hanks'),           'Life is like an array of chocolates.');
+INSERT INTO tweets (user_id, content) VALUES ((SELECT id from users where name='Grace Hopper'),        'It''s easier to ask forgiveness than it is to get permission.');
+INSERT INTO tweets (user_id, content) VALUES ((SELECT id from users where name='William Shakespeare'), 'to.be or to.not.be, that is the question.');
 INSERT INTO tweets (user_id, content) VALUES ((SELECT id from users where name='Beyonce'),             'If you liked it then you should have put a Promise on it.');
+INSERT INTO tweets (user_id, content) VALUES ((SELECT id from users where name='Tom Hanks'),           'Life is like an array of chocolates.');
 INSERT INTO tweets (user_id, content) VALUES ((SELECT id from users where name='Justin Bieber'),       'Is it too late now to say sorry? Cuz I''m missing more than just your <body></body>.');
 INSERT INTO tweets (user_id, content) VALUES ((SELECT id from users where name='Ash Ketchum'),         'char mander, I choose you!');
 INSERT INTO tweets (user_id, content) VALUES ((SELECT id from users where name='E.T.'),                'E.T. Slack Home.');
-INSERT INTO tweets (user_id, content) VALUES ((SELECT id from users where name='William Shakespeare'), 'to.be or to.not.be, that is the question.');
-INSERT INTO tweets (user_id, content) VALUES ((SELECT id from users where name='Grace Hopper'),        'It''s easier to ask forgiveness than it is to get permission.');
-INSERT INTO tweets (user_id, content) VALUES ((SELECT id from users where name='Kanye West'),          'I''ma let you finish coding, but…');
 INSERT INTO tweets (user_id, content) VALUES ((SELECT id from users where name='Taylor Swift'),        'I knew you were trouble when you logged in.');
-INSERT INTO tweets (user_id, content) VALUES ((SELECT id from users where name='Kanye West'),          'I think what Kanye West is going to mean is something similar to what Steve Jobs means.');
 INSERT INTO tweets (user_id, content) VALUES ((SELECT id from users where name='Taylor Swift'),        'I''ve got some whitespace baby — and I''ll write your `.name`');
+INSERT INTO tweets (user_id, content) VALUES ((SELECT id from users where name='Kanye West'),          'I think what Kanye West is going to mean is something similar to what Steve Jobs means.');
+INSERT INTO tweets (user_id, content) VALUES ((SELECT id from users where name='Kanye West'),          'I''ma let you finish coding, but…');
